cdcb6342b6f268dda3ffb84267be72aa81bcc3af
heikki-john <joni.tauriainen@gmail.com>
2015-10-08 14:02:49 +0300

    update

--- scripts/example_module.js
+++ scripts/example_module.js
@@ -10,4 +10,28 @@ my_module.controller('personController', ['$scope', function($scope){
 }]);
 */
 //tämä tapa ei ole suojattu minimointipakkaustyökalua vastaan
-my_module.controller('personController', function(){});
+my_module.controller('personController', function($scope){
+    
+    $scope.person = {};
+    $scope.person.some_data = "this is how data binding works!";
+    $scope.person.age = 32;
+    
+    
+    //ajax on funktio $ on sama kuin jquery
+    $.ajax({
+        method: "GET",
+        url: "http://localhost:28017/person/friends/",
+        dataType: "jsonp",
+        jsonp: "jsonp",
+        //success functio jos kysely onnistuu tjs. funktiolle pari parametriä voisi olla kolmekin, ei ole pakko.
+        success: function(data,status){
+
+        },
+        //kysely epäonnistuu
+        error: function(hrx, status, errorThrown){
+            console.log(errorThrown);
+        },
+    });    
+    
+    
+});
