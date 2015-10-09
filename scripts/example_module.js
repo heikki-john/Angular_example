90ad02f35b7f02e5dde13eebe967d555241411c2
heikki-john <joni.tauriainen@gmail.com>
2015-10-09 13:56:22 +0300

    update

--- scripts/example_module.js
+++ scripts/example_module.js
@@ -1,7 +1,23 @@
 //this is our first module
-var my_module = angular.module("root_module",[]);
+var my_module = angular.module("root_module",['ngRoute']);
 
 //After creating module you can add controllers, factories etc for it...
+
+//Configure routes
+my_module.config(function($routeProvider){
+    $routeProvider.when('/',{
+        controller:'personController',
+        templateUrl:'view1.html'
+    }).when('/friends',{
+        controller:'personController',
+        templateUrl:'view2.html'
+    }).when('/about',{
+        templateUrl:'view3.html'
+    }).otherwise({
+        redirectTo:'/'
+    });
+});
+
 
 //Tämä tapa on suojattu minimointipakkaustyökalua vastaan 
 /*
@@ -10,28 +26,44 @@ my_module.controller('personController', ['$scope', function($scope){
 }]);
 */
 //tämä tapa ei ole suojattu minimointipakkaustyökalua vastaan
-my_module.controller('personController', function($scope){
-    
+my_module.controller('personController', function($scope, personFactory){
     $scope.person = {};
-    $scope.person.some_data = "this is how data binding works!";
-    $scope.person.age = 32;
- 
-    $.ajax({
-        method: "GET",
-        url: "http://localhost:28017/person/friends/",
-        dataType: "jsonp",
-        jsonp: "jsonp",
-        //success functio jos kysely onnistuu tjs. funktiolle pari parametriä voisi olla kolmekin, ei ole pakko.
-        success: function(data,status){
-             $scope.$apply(function(){              // apply kertoo että data on latautunut 
-                $scope.person.headers = Object.keys(data.rows[0]);
-                $scope.person.actual_data = data.rows;
-            });
-        },
-        //kysely epäonnistuu
-        error: function(hrx, status, errorThrown){
-            console.log(errorThrown);
-        }
-    });    
-
+    $scope.person.data = [];
+    $scope.person.addPerson = function(){
+        console.log('You pressed save!');
+        var tempData = {
+            name: $scope.person.name,
+            address: $scope.person.address,
+            age: $scope.person.age
+        };
+        
+        $scope.person.data.push(tempData);
+        console.log($scope.person.data);
+    };
+    
 });
+
+//Create a factrory. Factory is sigleton, meaning there is only one instance of it.
+//jotta factoryn saa käyttöön controllerissa niin täytyy se laittaa kontrollorein parametriksi
+my_module.factory('personFactory', function(){
+    var my_factory = {};
+    my_factory.data = [];
+    
+    my_factory.addData = function(person){
+        my_factory.data.push(person);   
+    }
+    
+    my_factory.getData = function(){
+        return my_factory.data;
+    }
+    
+    return my_factory; //Factoryn pitää aina palauttaa factory objekti, muuten se ei toimi
+});
+
+
+
+
+
+
+
+
